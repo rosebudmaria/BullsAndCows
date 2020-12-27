0a161d675c8eeb056a1d5001b3da6eb590afedd6
rosebudmaria <73312741+rosebudmaria@users.noreply.github.com>
2020-12-27 07:23:57 -0800

    Update App.java

--- GuessTheNumber/src/main/java/GuessTheNumber/App.java
+++ GuessTheNumber/src/main/java/GuessTheNumber/App.java
@@ -1,20 +1,16 @@
 
-
-package GuessTheNumber;
+package com.mycompany.guessthenumber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
- * @author Shantoria Taylor  ,  Dec 25, 2020  ,  8:33:09 PM
-
+ * @author rosalindapowell0608
  */
-
 @SpringBootApplication
 public class App {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
 }
