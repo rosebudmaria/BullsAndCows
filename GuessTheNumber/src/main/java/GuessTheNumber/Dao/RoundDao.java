d69b54a4372e646d620543ee649c48788d9439c8
rosebudmaria <73312741+rosebudmaria@users.noreply.github.com>
2020-12-27 07:16:24 -0800

    Update RoundDao.java

--- GuessTheNumber/src/main/java/GuessTheNumber/Dao/RoundDao.java
+++ GuessTheNumber/src/main/java/GuessTheNumber/Dao/RoundDao.java
@@ -1,24 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package GuessTheNumber.Dao;
+package com.mycompany.guessthenumber.data;
 
-import GuessTheNumber.dto.Round;
+import com.mycompany.guessthenumber.models.Round;
 import java.util.List;
 
 /**
  *
- * @author shana
+ * @author rosalindapowell0608
  */
 public interface RoundDao {
-    
-    public Round AddRound (Round newRound);
-    
-    public List<Round> GetRoundByTime (int gameId);
-    
-    public void DeleteRoundById(int roundId);
-    
+   public Round add(Round newRound);
+   //creates guess b passing guess and gameid in as JSON
+   //program must calculate results & mark finished if correct
+   //returns Round object with results included
+   
+   public List<Round> getAll(int GameId);
+   //returns list of rounds for specific game sorted by time
+   
+   public void deleteRoundId(int RoundId);
+   
+   
     
 }
