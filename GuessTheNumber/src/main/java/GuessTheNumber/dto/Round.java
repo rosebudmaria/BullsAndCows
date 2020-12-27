ae43298dc097c3304182afd7fed7c0f81f509485
rosebudmaria <73312741+rosebudmaria@users.noreply.github.com>
2020-12-27 07:17:33 -0800

    Update Round.java

--- GuessTheNumber/src/main/java/GuessTheNumber/dto/Round.java
+++ GuessTheNumber/src/main/java/GuessTheNumber/dto/Round.java
@@ -1,101 +1,56 @@
 
 
-package GuessTheNumber.dto;
-
-import java.time.LocalDateTime;
-import java.util.Objects;
+package com.mycompany.guessthenumber.models;
 
 /**
  *
- * @author Shantoria Taylor  ,  Dec 25, 2020  ,  8:37:20 PM
-
+ * @author rosalindapowell0608
  */
 public class Round {
-
+    private int RoundId;
+    private int GameId;
+    private String Guess;
+    private String TimeStampOfRound;
+    private String Result;
+    
     public int getRoundId() {
         return RoundId;
     }
-
+    
     public void setRoundId(int RoundId) {
         this.RoundId = RoundId;
     }
-
-    public Game getGameId() {
+    
+    
+    public int getGameId() {
         return GameId;
     }
-
-    public void setGameId(Game GameId) {
+    
+    public void setGameId(int GameId) {
         this.GameId = GameId;
     }
-
-    public char getGuess() {
+    
+    
+    public String getGuess() {
         return Guess;
     }
-
-    public void setGuess(char Guess) {
+    
+    public void setGuess(String Guess) {
         this.Guess = Guess;
     }
-
-    public LocalDateTime getTimeStampOfRound() {
+    
+    public String getTimeStampOfRound() {
         return TimeStampOfRound;
     }
-
-    public void setTimeStampOfRound(LocalDateTime TimeStampOfRound) {
+    
+    public void setTimeStampOfRound(String TimeStampOfRound) {
         this.TimeStampOfRound = TimeStampOfRound;
     }
-
     public String getResult() {
         return Result;
     }
-
+    
     public void setResult(String Result) {
         this.Result = Result;
     }
-    private int RoundId;
-    public Game GameId;
-    private char Guess;
-    private LocalDateTime TimeStampOfRound;
-    private String Result;
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 71 * hash + this.RoundId;
-        hash = 71 * hash + Objects.hashCode(this.GameId);
-        hash = 71 * hash + this.Guess;
-        hash = 71 * hash + Objects.hashCode(this.TimeStampOfRound);
-        hash = 71 * hash + Objects.hashCode(this.Result);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Round other = (Round) obj;
-        if (this.RoundId != other.RoundId) {
-            return false;
-        }
-        if (this.Guess != other.Guess) {
-            return false;
-        }
-        if (!Objects.equals(this.Result, other.Result)) {
-            return false;
-        }
-        if (!Objects.equals(this.GameId, other.GameId)) {
-            return false;
-        }
-        if (!Objects.equals(this.TimeStampOfRound, other.TimeStampOfRound)) {
-            return false;
-        }
-        return true;
-    }
-
 }
