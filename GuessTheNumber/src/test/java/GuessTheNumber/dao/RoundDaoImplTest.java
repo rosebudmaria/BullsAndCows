95a70ed98400622faa2167d95769ec66f5dfc093
rosebudmaria <73312741+rosebudmaria@users.noreply.github.com>
2020-12-29 11:00:30 -0800

    Update RoundDaoImplTest.java

--- GuessTheNumber/src/test/java/GuessTheNumber/dao/RoundDaoImplTest.java
+++ GuessTheNumber/src/test/java/GuessTheNumber/dao/RoundDaoImplTest.java
@@ -1,26 +1,29 @@
 package GuessTheNumber.dao;
+
 import GuessTheNumber.dto.Game;
+import GuessTheNumber.GuessTheNumberApplicationTests;
 import GuessTheNumber.dto.Round;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
-
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.runner.RunWith;
+
 /**
  *
- * @author Rosalinda Powell
+ * @author rosalindapowell0608
  */
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = GuessTheNumberApplicationTests.class)
 public class RoundDaoImplTest {
     
     @Autowired 
@@ -32,8 +35,9 @@ public class RoundDaoImplTest {
     private Game game;
     private Round round;
     private Round testRound;
+    
     @BeforeEach
-    public void setUpClass() throws Exception {
+    public void setUp() throws Exception {
         List<Game>  gameList = gameDao.getListOfGames();
         for(Game game : gameList) {
             gameDao.deleteGameById (game.getGameId());
@@ -48,5 +52,21 @@ public class RoundDaoImplTest {
         Round round = roundDao.getGameById(testRound.getRoundId());
         assertNotNull(testRound, "The game should not be null.");
         assertEquals(testRound, round);
+    }
+    
+    
+   @Test
+    public void testUpdate() {
+        Round round = new Round();
+        round.setRoundId(2);
+        round.setGuess("1234");
+        round = roundDao.addRound(round);
+        
+        Round roundDao = roundDao.getRoundById(round.getRoundId());
+        
+    }
+
+    private void assertNotNull(Round testRound, String the_game_should_not_be_null) {
+        throw new UnsupportedOperationException("The game should not be null."); //To change body of generated methods, choose Tools | Templates.
     }
 }
